e95bee99aed4caacd980439133755760595eb5ac
Sora1829 <49411352+Sora1829@users.noreply.github.com>
2019-11-20 18:07:47 -0600

    Initial commit

---
+++ RockPaperScissors/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace RockPaperScissors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Rock = new System.Windows.Forms.Button();
+            this.Paper = new System.Windows.Forms.Button();
+            this.Scissors = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Rock
+            // 
+            this.Rock.Location = new System.Drawing.Point(39, 144);
+            this.Rock.Name = "Rock";
+            this.Rock.Size = new System.Drawing.Size(186, 118);
+            this.Rock.TabIndex = 0;
+            this.Rock.Text = "Rock";
+            this.Rock.UseVisualStyleBackColor = true;
+            this.Rock.Click += new System.EventHandler(this.Rock_Click);
+            // 
+            // Paper
+            // 
+            this.Paper.Location = new System.Drawing.Point(305, 144);
+            this.Paper.Name = "Paper";
+            this.Paper.Size = new System.Drawing.Size(186, 118);
+            this.Paper.TabIndex = 1;
+            this.Paper.Text = "Paper";
+            this.Paper.UseVisualStyleBackColor = true;
+            this.Paper.Click += new System.EventHandler(this.Paper_Click);
+            // 
+            // Scissors
+            // 
+            this.Scissors.Location = new System.Drawing.Point(563, 144);
+            this.Scissors.Name = "Scissors";
+            this.Scissors.Size = new System.Drawing.Size(186, 118);
+            this.Scissors.TabIndex = 2;
+            this.Scissors.Text = "Scissors";
+            this.Scissors.UseVisualStyleBackColor = true;
+            this.Scissors.Click += new System.EventHandler(this.Scissors_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(202, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(405, 82);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Choose your weapon!!!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Scissors);
+            this.Controls.Add(this.Paper);
+            this.Controls.Add(this.Rock);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Rock;
+        private System.Windows.Forms.Button Paper;
+        private System.Windows.Forms.Button Scissors;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RockPaperScissors/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RockPaperScissors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int Player;
+        Random rand = new Random();
+        string[]  vs= new string[]{"Rock","Paper","Scissors"};
+
+        private void Rock_Click(object sender, EventArgs e)
+        {
+            Player = 0;
+            WinCheck();
+
+        }
+
+        private void Paper_Click(object sender, EventArgs e)
+        {
+            Player = 1;
+            WinCheck();
+        }
+
+        private void Scissors_Click(object sender, EventArgs e)
+        {
+            Player = 2;
+            WinCheck();
+        }
+
+        //0-win 1-lose 2-Tie
+
+        private void WinCheck()
+        {
+            int ai = rand.Next(3);
+
+            int stat;
+
+            if (Player == ai)
+            {
+                stat = 2;
+            }
+            else if (Player == 0 && ai == 2)
+            {
+                stat = 0;
+            }
+            else if (ai == 0 && Player == 2)
+            {
+                stat = 1;
+            }
+            else if (Player > ai)
+            {
+                stat = 0;
+            }
+            else
+            {
+                stat = 1;
+            }
+
+
+
+
+            if (stat == 0)
+            {
+                this.label1.Text = $"You Won AI Chose {vs[ai]}";
+            }
+            else if (stat == 1)
+            {
+                this.label1.Text = $"You Lost AI Chose {vs[ai]}";
+            }
+            else
+            {
+                this.label1.Text = $"You Tied AI Chose {vs[ai]}";
+            }
+        }
+    }
+}
